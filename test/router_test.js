ed00bf7537af13e2a9a61c22e9e3ef0b8cf683b1
Ante Salinovic <antes@qti.qualcomm.com>
2015-07-23 19:29:46 +1000

    Fixed up route atom initialisation and tests.

--- test/router_test.js
+++ test/router_test.js
@@ -2,13 +2,14 @@
 
 var test = require('tape');
 var document = require('global/document');
-var router = require('../lib/router');
+
+document.location = { href: '/articles?page=25' };
+
+var atom = require('../lib/router').atom;
 
 test('router', function test(t) {
-    document.location = { href: '/articles?page=25' };
-
     t.equal(
-        router().state(),
+        atom(),
         document.location.href,
         'router init with full url, including query string');
 
