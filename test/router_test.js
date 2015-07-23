a652add3edb1e9680daa8c5332ef17e5e60b1b2c
Ante Salinovic <antes@qti.qualcomm.com>
2015-07-23 17:58:57 +1000

    Added eslint and linted files.

--- test/router_test.js
+++ test/router_test.js
@@ -1,14 +1,16 @@
 'use strict';
 
+var test = require('tape');
+var document = require('global/document');
 var router = require('../lib/router');
 
 test('router', function test(t) {
-  document.location = { href: "/articles?page=25" }
+    document.location = { href: '/articles?page=25' };
 
-  t.equal(
-  	router().state(),
-  	document.location.href,
-  	'router init with full url, including query string')
+    t.equal(
+        router().state(),
+        document.location.href,
+        'router init with full url, including query string');
 
-  t.end()
+    t.end();
 });
--- test/test.js
+++ test/test.js
@@ -1,14 +1,12 @@
 'use strict';
 
-global.test = require('tape');
-
 global.window = require('global/window');
 global.window.addEventListener = function polyAddEventListneer() {
-  return false;
-}
+    return false;
+};
 
 global.document = require('global/document');
-global.document.location = { href: "/" };
+global.document.location = { href: '/' };
 
 require('./index_test.js');
 require('./router_test.js');
