bf9b790d495db653718c96bf476626e57afda0ac
Ante Salinovic <antes@qti.qualcomm.com>
2015-08-28 15:00:16 +1000

    Simplified tests, moved back into a single file.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,32 @@
 'use strict';
 
-global.window = require('global/window');
-global.window.addEventListener = function polyAddEventListneer() {
-    return false;
-};
+var test = require('tape');
+var window = require('global/window');
+var document = require('global/document');
 
-global.document = require('global/document');
-global.document.location = { href: '/' };
+window.addEventListener = function polyAddEventListneer() { return false; };
+document.location = { href: '/articles?page=25' };
 
-require('./index_test.js');
-require('./router_test.js');
+var router = require('../index');
+var atom = require('../lib/router').atom;
+
+
+test('init', function test(t) {
+    var state = router();
+
+    t.ok(router.render, 'has render function');
+    t.ok(router.anchor, 'has anchor function');
+    t.ok(state, 'state init');
+    t.end();
+});
+
+test('router', function test(t) {
+    var state = router();
+
+    t.equal(
+        atom(),
+        document.location.href,
+        'router init with full url, including query string');
+
+    t.end();
+});
