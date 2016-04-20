63e1fe9c4f0d06f4b4938ea55a07c8427133331f
Ante Salinovic <nikuda@gmail.com>
2016-04-20 13:48:26 +1000

    Added example code to lint path

--- example/index.js
+++ example/index.js
@@ -6,44 +6,44 @@ var router = require('../');
 var anchor = router.anchor;
 
 function App() {
-  var state = mercury.state({
-    title: mercury.value('Router Example'),
-    route: router()
-  });
-  return state;
+    var state = mercury.state({
+        title: mercury.value('Router Example'),
+        route: router()
+    });
+    return state;
 }
 
 mercury.app(document.body, App(), render);
 
 function render(state) {
-  return h('div', [
-    h('h1', [state.title]),
-    menu(),
-    router.render(state, {
-      '/': function() {
-        return h('h1', ['Home']);
-      },
-      '/animals': function() {
-        return h('h1', ['Animals']);
-      },
-      '/animals/:id': function(params) {
-        return h('h1', ['Animals ' + params.id]);
-      }
-    })
-  ]);
+    return h('div', [
+        h('h1', [state.title]),
+        menu(),
+        router.render(state, {
+            '/': function routeHome() {
+                return h('h1', ['Home']);
+            },
+            '/animals': function routeAnimals() {
+                return h('h1', ['Animals']);
+            },
+            '/animals/:id': function routeAnimal(params) {
+                return h('h1', ['Animals ' + params.id]);
+            }
+        })
+    ]);
 }
 
 function menu() {
-  return h('ul', [
-    h('li', [
-      anchor({ href: '/'}, 'Home')
-    ]),
-    h('li', [
-      anchor({ href: '/animals' }, 'Animals')
-    ]),
-    h('li', [
-      anchor({ href: '/animals/1'}, 'Animals1')
-    ])
-  ]);
+    return h('ul', [
+        h('li', [
+            anchor({ href: '/'}, 'Home')
+        ]),
+        h('li', [
+            anchor({ href: '/animals' }, 'Animals')
+        ]),
+        h('li', [
+            anchor({ href: '/animals/1'}, 'Animals1')
+        ])
+    ]);
 }
 
